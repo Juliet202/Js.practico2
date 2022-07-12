95b8cdd65f57c7937a568d20046387d0751f64ac
Julieta <valdezjulieta45@gmail.com>
2022-07-11 23:42:48 -0300

    inicie ejercicio 4

---
+++ js/ejercicio4.js
@@ -0,0 +1,9 @@
+let suma = 0; 
+
+while (confirm('Desea ingresar un numero ?')){
+    let numero = prompt('ingrese un numero');
+    (Number(numero)== numero)
+    numero = Number (numero)
+    suma = suma + numero
+        console.log(suma)
+}
